e64d97e222abdb32322a631c92818c7fc1d13f1a
levhyun <g88080997@gmail.com>
2022-11-22 08:32:50 +0900

    commit

--- BronzeIV/11720.c
+++ BronzeIV/11720.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    
+    int n, res=0;
+    scanf("%d", &n);
+    char c[n];
+    scanf("%s", &c);
+    for(int i = 0; i < n; i++){
+        res+=c[i]-'0';
+    }
+    printf("%d", res);
 }
